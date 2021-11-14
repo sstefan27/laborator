c62a3cb63acfcc34eb98f538eb80dc431f2aec90
stefann27 <stefanbaiduc@gmail.com>
2021-11-14 12:48:39 +0200

    just created the console app

---
+++ stefan_baiduc/L05/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace L05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
